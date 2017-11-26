d16799b643d89acd3cec882db4f97623e5cca07d
Arthur van der Peijl <aavanderpeijl@gmail.com>
2017-11-26 20:05:17 +0000

    First deployment

--- copy/var/zoneinit/includes/15-mdata-setup.sh
+++
@@ -1,2 +0,0 @@
-# Run mdata setup on provision state
-/opt/local/bin/mdata-setup
--- copy/var/zoneinit/includes/16-ndpd-log-spam.sh
+++
@@ -1,7 +0,0 @@
-# https://github.com/joyent/smartos-live/issues/501
-if mdata-get sdc:nics | grep -qv addrconf 1>/dev/null 2>&1; then
-	echo '# https://github.com/joyent/smartos-live/issues/501' > /etc/inet/ndpd.conf
-	echo 'ifdefault StatelessAddrConf off'                    >> /etc/inet/ndpd.conf
-else
-	echo > /etc/inet/ndpd.conf
-fi
